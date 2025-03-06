3fcf70b4fc1d99e5c121cfde18d2a0d13293e8c6
cartdb <nguyennamtran2022@gmail.com>
2025-03-06 13:01:41 -0600

    Update

--- data.py
+++ data.py
@@ -1,4 +1,8 @@
 import gspread
-gc = gspread.service_account("../keys.json").open("Price Comparison Form (Responses)")
-wks = gc.sheet1.get_all_values()
-print(wks)
+import random
+arr = list("abcdefghijklmnopqrstuvwxyz0123456789")
+gc = gspread.service_account("../keys.json").open("Price Comparison Form (Responses)").sheet1
+wks = gc.get_all_values()
+for r in range(len(wks)):
+    if wks[r][5] == "":
+        gc.update_acell("F" + str(r+1), "".join([arr[random.randint(0, 35)] for c in range(16)]))
